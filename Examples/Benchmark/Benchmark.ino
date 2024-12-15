d7a3142262ba449ab67daaa73c52bb001fc57bb2
GitMoDu <batatas@gmail.com>
2024-12-15 19:51:14 +0000

    Added Benchmark, to track improvements/regressions.

---
+++ Examples/Benchmark/Benchmark.ino
@@ -0,0 +1,98 @@
+
+#define SERIAL_BAUD_RATE 115200
+
+#include <Arduino.h>
+#include <IntegerSignal.h>
+#include "Benchmarks.h"
+
+using namespace IntegerSignal;
+
+static constexpr uint16_t TestSize = 2000;
+
+
+void setup()
+{
+	Serial.begin(SERIAL_BAUD_RATE);
+	while (!Serial)
+		;
+	delay(1000);
+
+	Serial.println();
+	Serial.println();
+	Serial.println(F("Integer Signals Benchmark"));
+	Serial.print('\t');
+	PrintPlaform();
+	Serial.println();
+	Serial.println();
+
+	Serial.println(F("Average Step Duration (ns)"));
+	Serial.println();
+
+	Serial.println(F("Filters"));
+	Serial.println(F("\tVirtual Interface"));
+	BenchmarkFilters<Filter::IFilter<uint8_t>, Filter::IFilter<uint16_t>, Filter::IFilter<uint32_t>, TestSize>();
+	Serial.println(F("\tLowPass"));
+	BenchmarkFilters<Filters::LowPassU8<>, Filters::LowPassU16<>, Filters::LowPassU32<>, TestSize>();
+	Serial.println(F("\tEMA"));
+	BenchmarkFilters<Filters::EmaU8<>, Filters::EmaU16<>, Filters::EmaU32<>, TestSize>();
+	Serial.println(F("\tDEMA"));
+	BenchmarkFilters<Filters::DemaU8<>, Filters::DemaU16<>, Filters::DemaU32<>, TestSize>();
+	Serial.println();
+
+	Serial.println(F("Curves"));
+	Serial.println(F("\tVirtual Interface"));
+	BenchmarkCurves<IntegerSignal::Curve::ICurve<uint8_t>, IntegerSignal::Curve::ICurve<uint16_t>, IntegerSignal::Curve::ICurve<uint32_t>, TestSize>();
+	Serial.println(F("\tLimit"));
+	BenchmarkCurves<Curves::LimitU8<0, INT8_MAX>, Curves::LimitU16<0, INT16_MAX>, Curves::LimitU32<0, INT32_MAX>, TestSize>();
+	Serial.println(F("\tScaleDown"));
+	BenchmarkCurves<Curves::ScaleDownU8<INT8_MAX>, Curves::ScaleDownU16<INT8_MAX>, Curves::ScaleDownU32<INT8_MAX>, TestSize>();
+	Serial.println(F("\tPower2"));
+	BenchmarkCurves<Curves::Power2U8<>, Curves::Power2U16<>, Curves::Power2U32<>, TestSize>();
+	Serial.println(F("\tPower3"));
+	BenchmarkCurves<Curves::Power3U8<>, Curves::Power3U16<>, Curves::Power3U32<>, TestSize>();
+	Serial.println(F("\tRoot2"));
+	BenchmarkCurves<Curves::Root2U8<>, Curves::Root2U16<>, Curves::Root2U32<>, TestSize>();
+	Serial.println();
+
+	Serial.println(F("Mix"));
+	BenchmarkMix();
+	Serial.println();
+
+	Serial.println(F("Math"));
+	BenchmarkSquareRoot();
+	Serial.println();
+}
+
+void loop()
+{
+}
+
+
+void PrintPlaform()
+{
+#if defined(ARDUINO_ARCH_AVR)
+	Serial.print(F("AVR"));
+#elif defined(ARDUINO_ARCH_STM32F1) || defined(ARDUINO_ARCH_STM32)
+	Serial.print(F("STM32 F1"));
+#elif defined(ARDUINO_ARCH_STM32F4)
+	Serial.print(F("STM32 F4"));
+#elif defined(ARDUINO_ARCH_RP2040)
+#if defined(PICO_RP2350)
+	Serial.print(F("RP2350"));
+#else
+	Serial.print(F("RP2040"));
+#endif
+#elif defined(ARDUINO_ARCH_NRF52)
+	Serial.print(F("NRF52840"));
+#elif defined(ARDUINO_ARCH_ESP32)
+	Serial.print(F("ESP32"));
+#endif
+
+	Serial.print(F(" @ "));
+#if defined(ARDUINO_ARCH_STM32F4)
+	Serial.print(CLOCK_SPEED_MHZ);
+#else
+	Serial.print(F_CPU / 1000000);
+#endif
+	Serial.println(F(" MHz"));
+}
---
+++ Examples/Benchmark/Benchmarks.h
@@ -0,0 +1,275 @@
+// Benchmarks.h
+
+#ifndef _BENCHMARKS_h
+#define _BENCHMARKS_h
+
+#include <IntegerSignal.h>
+#include <Arduino.h>
+
+
+template<typename Filter8, typename Filter16, typename Filter32, uint16_t TestSize = 2000>
+void BenchmarkFilters()
+{
+	Filter8 filter8{};
+	Filter16 filter16{};
+	Filter32 filter32{};
+
+	volatile uint8_t testU8 = UINT8_MAX;
+	volatile uint16_t testU16 = UINT16_MAX;
+	volatile uint32_t testU32 = UINT32_MAX;
+
+	filter8.Clear();
+	filter16.Clear();
+	filter32.Clear();
+
+	filter8.Set(UINT8_MAX);
+	filter16.Set(UINT16_MAX);
+	filter32.Set(UINT32_MAX);
+
+	uint32_t DurationTotal = 0;
+
+	Serial.print(F("\t(8 Bit)\t\t"));
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		filter8.Step();
+		testU8 = filter8.Get();
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(16 Bit)\t"));
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		filter16.Step();
+		testU16 = filter16.Get();
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(32 Bit)\t"));
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		filter32.Step();
+		testU32 = filter32.Get();
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+	Serial.println();
+}
+
+template<typename Curve8, typename Curve16, typename Curve32, uint16_t TestSize = 2000>
+void BenchmarkCurves()
+{
+	Curve8 curve8{};
+	Curve16 curve16{};
+	Curve32 curve32{};
+
+	volatile uint8_t testU8 = UINT8_MAX;
+	volatile uint16_t testU16 = UINT16_MAX;
+	volatile uint32_t testU32 = UINT32_MAX;
+
+	uint32_t DurationTotal = 0;
+
+	Serial.print(F("\t(8 Bit)\t\t"));
+	testU8 = UINT8_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU8 = curve8.Get(testU8);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(16 Bit)\t"));
+	testU16 = UINT16_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU16 = curve16.Get(testU16);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(32 Bit)\t"));
+	testU32 = UINT32_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU32 = curve32.Get(testU32);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+	Serial.println();
+}
+
+template<uint16_t TestSize = 2000>
+void BenchmarkMix()
+{
+	volatile uint8_t testU8 = UINT8_MAX;
+	volatile uint16_t testU16 = UINT16_MAX;
+	volatile uint32_t testU32 = UINT32_MAX;
+
+	uint32_t DurationTotal = 0;
+
+	Serial.println(F("\tLinear 50/50"));
+	Serial.print(F("\t(8 Bit)\t\t"));
+	testU8 = UINT8_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU8 = IntegerSignal::Mix::LinearU8(testU8, UINT8_MAX);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(16 Bit)\t"));
+	testU16 = UINT16_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU16 = IntegerSignal::Mix::LinearU16(testU16, UINT16_MAX);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(32 Bit)\t"));
+	testU32 = UINT32_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU32 = IntegerSignal::Mix::LinearU32(testU32, UINT32_MAX);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+	Serial.println();
+
+	Serial.println(F("\tLinear [0;255]"));
+	Serial.print(F("\t(8 Bit)\t\t"));
+	testU8 = UINT8_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU8 = IntegerSignal::Mix::LinearU8(testU8, UINT8_MAX, (uint8_t)i);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(16 Bit)\t"));
+	testU16 = UINT16_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU16 = IntegerSignal::Mix::LinearU16(testU16, UINT16_MAX, (uint8_t)i);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(32 Bit)\t"));
+	testU32 = UINT32_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU32 = IntegerSignal::Mix::LinearU32(testU32, UINT32_MAX, (uint8_t)i);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+	Serial.println();
+
+
+	Serial.println(F("\tPower 50/50"));
+	Serial.print(F("\t(8 Bit)\t\t"));
+	testU8 = UINT8_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU8 = IntegerSignal::Mix::Power2U8(testU8, UINT8_MAX);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(16 Bit)\t"));
+	testU16 = UINT16_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU16 = IntegerSignal::Mix::Power2U16(testU16, UINT16_MAX);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(32 Bit)\t"));
+	testU32 = UINT32_MAX;
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU32 = IntegerSignal::Mix::Power2U32(testU32, UINT32_MAX);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+	Serial.println();
+}
+
+template<uint16_t TestSize = 2000>
+void BenchmarkSquareRoot()
+{
+	volatile uint16_t testU16 = UINT16_MAX;
+	volatile uint32_t testU32 = UINT32_MAX;
+	volatile uint64_t testU64 = UINT64_MAX;
+
+	uint32_t DurationTotal = 0;
+
+	Serial.println(F("\tSquare Root (Root2)"));
+	Serial.print(F("\t(16 Bit)\t"));
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU16 = IntegerSignal::Math::Root2U16(testU16);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(32 Bit)\t"));
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU32 = IntegerSignal::Math::Root2U32(testU32);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+
+	Serial.print(F("\t(64 Bit)\t"));
+	DurationTotal = micros();
+	for (uint16_t i = 0; i < TestSize; i++)
+	{
+		testU64 = IntegerSignal::Math::Root2U64(testU64);
+	}
+	DurationTotal = micros() - DurationTotal;
+	Serial.print((DurationTotal * 1000L) / TestSize);
+	Serial.println(F(" ns"));
+	Serial.println();
+}
+
+
+
+#endif
+
