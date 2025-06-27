92e647e8afe930c119523a39d049c0ddeaf6efc2
GitMoDu <batatas@gmail.com>
2025-06-27 15:09:44 +0100

    Added comments.

--- src/Base/Size.h
+++ src/Base/Size.h
@@ -5,32 +5,62 @@
 
 namespace IntegerSignal
 {
+	/// <summary>
+	/// BitSize provides utilities for compile-time bit and byte calculations,
+	/// as well as type selection based on required storage size.
+	/// </summary>
 	namespace BitSize
 	{
+		/// <summary>
+		/// Compile-time constant value.
+		/// </summary>
+		/// <typeparam name="N">The constant value.</typeparam>
 		template<uint64_t N>
 		struct constant
 		{
 			enum { value = N };
 		};
 
+		/// <summary>
+		/// Helper for returning a type.
+		/// </summary>
+		/// <typeparam name="T">The type to return.</typeparam>
 		template<typename T>
 		struct return_type
 		{
 			typedef T type;
 		};
 
+		/// <summary>
+		/// Computes the number of bits required to represent N.
+		/// </summary>
+		/// <typeparam name="N">The value to analyze.</typeparam>
 		template<uint64_t N>
 		struct bit_count : constant<1 + bit_count<(N >> 1)>::value> {};
 
+		/// <summary>
+		/// Specialization for N = 0.
+		/// </summary>
 		template<>
 		struct bit_count<0> : constant<1> {};
 
+		/// <summary>
+		/// Specialization for N = 1.
+		/// </summary>
 		template<>
 		struct bit_count<1> : constant<1> {};
 
+		/// <summary>
+		/// Computes the number of bytes required to represent N.
+		/// </summary>
+		/// <typeparam name="N">The value to analyze.</typeparam>
 		template<uint64_t N>
 		struct byte_count : constant<((bit_count<N>::value + 7) >> 3)> {};
 
+		/// <summary>
+		/// Selects an unsigned integer type with at least the specified number of bytes.
+		/// </summary>
+		/// <typeparam name="N">Number of bytes required.</typeparam>
 		template<uint64_t N>
 		struct byte_type : BitSize::return_type<uint64_t> {};
 
@@ -46,6 +76,9 @@ namespace IntegerSignal
 		template<>
 		struct byte_type<1> : BitSize::return_type<uint8_t> {};
 
+		/// <summary>
+		/// Selects the smallest uint_least type with at least the specified number of bytes.
+		/// </summary>
 		template<uint64_t N>
 		struct byte_least_type : BitSize::return_type<uint_least64_t> {};
 
@@ -61,6 +94,9 @@ namespace IntegerSignal
 		template<>
 		struct byte_least_type<1> : BitSize::return_type<uint_least8_t> {};
 
+		/// <summary>
+		/// Selects the fastest uint_fast type with at least the specified number of bytes.
+		/// </summary>
 		template<uint64_t N>
 		struct byte_fast_type : BitSize::return_type<uint_fast64_t> {};
 
@@ -77,28 +113,47 @@ namespace IntegerSignal
 		struct byte_fast_type<1> : BitSize::return_type<uint_fast8_t> {};
 	}
 
+	/// <summary>
+	/// UIntSize provides a type alias for the smallest unsigned integer type
+	/// that can store values up to N.
+	/// </summary>
 	namespace UIntSize
 	{
-		template<uint64_t N>
+		/// <summary>
+		/// Selects the smallest unsigned integer type that can store values up to N.
+		/// </summary>
+		/// <typeparam name="N">Maximum value to store.</typeparam>
 		struct uint_type : BitSize::byte_type<BitSize::byte_count<N>::value> {};
 	}
 
+	/// <summary>
+	/// UIntLeastSize provides a type alias for the smallest uint_least type
+	/// that can store values up to N.
+	/// </summary>
 	namespace UIntLeastSize
 	{
-		template<uint64_t N>
+		/// <summary>
+		/// Selects the smallest uint_least type that can store values up to N.
+		/// </summary>
+		/// <typeparam name="N">Maximum value to store.</typeparam>
 		struct uint_least_type : BitSize::byte_least_type<BitSize::byte_count<N>::value> {};
 	}
 
+	/// <summary>
+	/// UIntFastSize provides a type alias for the fastest uint_fast type
+	/// that can store values up to N.
+	/// </summary>
 	namespace UIntFastSize
 	{
-		template<uint64_t N>
+		/// <summary>
+		/// Selects the fastest uint_fast type that can store values up to N.
+		/// </summary>
+		/// <typeparam name="N">Maximum value to store.</typeparam>
 		struct uint_fast_type : BitSize::byte_fast_type<BitSize::byte_count<N>::value> {};
 	}
 
 	using namespace UIntSize;
-
 	using namespace UIntLeastSize;
-
 	using namespace UIntFastSize;
 }
 #endif
