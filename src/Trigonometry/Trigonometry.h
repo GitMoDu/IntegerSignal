dc2f4abc5e4d148c819d603d8eb744d9a962397b
GitModu <batatas@gmail.com>
2026-03-31 15:07:52 +0100

    Clean-up angle trigonometry.

--- src/Trigonometry/Trigonometry.h
+++ src/Trigonometry/Trigonometry.h
@@ -3,7 +3,6 @@
 
 #include <stdint.h>
 
-
 // Define INTEGER_TRIGONOMETRY_LUT as INTEGER_TRIGONOMETRY_LUT_TINY to use the 64 items LUT.
 #define INTEGER_TRIGONOMETRY_LUT_TINY 1
 
@@ -28,9 +27,13 @@ namespace IntegerSignal
 		/// Modular fixed-point angle for a full rotation.
 		/// - Domain: [0; ANGLE_RANGE], where ANGLE_RANGE == UINT16_MAX.
 		/// - Semantics: values wrap modulo (ANGLE_RANGE + 1). 0 and ANGLE_RANGE + 1 represent the same angle.
+		/// - Precision: 16 bits for the full rotation, which gives a resolution of approximately 0.0055 degrees per unit.
 		/// </summary>
 		typedef uint16_t angle_t;
 
+		/// <summary>
+		/// Maximum range for angle values, set to the maximum value of a 16-bit unsigned integer (65535).
+		/// </summary>
 		static constexpr angle_t ANGLE_RANGE = UINT16_MAX;
 
 		/// <summary>
@@ -41,7 +44,7 @@ namespace IntegerSignal
 		/// <returns>Modular angle in [0; ANGLE_RANGE].</returns>
 		static constexpr angle_t GetAngle(const int16_t degrees)
 		{
-			return ((int32_t)degrees * ((int32_t)ANGLE_RANGE + 1)) / 360;
+			return (((int32_t)degrees * ((int32_t)ANGLE_RANGE + 1)) / 360) & ANGLE_RANGE;
 		}
 
 		static constexpr angle_t ANGLE_90 = GetAngle(90);
